95bc60b06925d1270182b2e035a4e6c83c757721
Akash <subs@appsparkler.com>
2019-06-28 12:46:45 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 
 // import all required libraries
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize.min.js'
 //
 import Navbar from './components/Navbar'
 //
 import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   return (
-    <BrowserRouter>
       <div className='App'>
         <Navbar />
+        <BrowserRouter>
+          <Route exact path="/" component={Home} />
+          <Route path="/contact" component={Contact}/>
+          <Route path="/about" component={About} />
+        </BrowserRouter>
       </div>
-    </BrowserRouter>
   );
 }
 
--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const About =() => {
-    return {
+    return (
       <div className="container">
-        <h4 className="center">About Page</h4>
+        <h4 className="center">About</h4>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo sit accusamus ducimus quod possimus, quas necessitatibus laborum? Inventore error hic cupiditate saepe quasi quidem facilis, temporibus vero harum, repellat ab, nostrum corporis voluptatibus magni dignissimos delectus tempora? Animi consequatur modi rerum aperiam ea magni, quos, soluta suscipit voluptatum officiis at explicabo consectetur dolor aliquid quidem mollitia. Enim quia nemo aperiam quidem incidunt consequuntur. Consequuntur ut amet tenetur iure modi itaque nostrum, nam alias voluptates officiis atque aliquam velit vero repellendus molestias odit ducimus quia. Velit earum ab, magnam tempora perferendis mollitia sapiente sint, aliquid consequuntur eum unde dicta, repellendus voluptas!
         </p>
       </div>
-    }
+    )
 }
 
 export default About;
