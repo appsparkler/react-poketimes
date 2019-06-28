04208f497f9cfc1679ea61a2011ad96e286bb236
Akash <subs@appsparkler.com>
2019-06-28 12:25:01 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 
+
+
 function App() {
   return (
     <h1><i>Poke Times Of India...</i></h1>
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Home =() => {
+const About =() => {
     return {
       <div className="container">
-        <h4 className="center">Home Page</h4>
+        <h4 className="center">About Page</h4>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo sit accusamus ducimus quod possimus, quas necessitatibus laborum? Inventore error hic cupiditate saepe quasi quidem facilis, temporibus vero harum, repellat ab, nostrum corporis voluptatibus magni dignissimos delectus tempora? Animi consequatur modi rerum aperiam ea magni, quos, soluta suscipit voluptatum officiis at explicabo consectetur dolor aliquid quidem mollitia. Enim quia nemo aperiam quidem incidunt consequuntur. Consequuntur ut amet tenetur iure modi itaque nostrum, nam alias voluptates officiis atque aliquam velit vero repellendus molestias odit ducimus quia. Velit earum ab, magnam tempora perferendis mollitia sapiente sint, aliquid consequuntur eum unde dicta, repellendus voluptas!
         </p>
@@ -11,4 +11,4 @@ const Home =() => {
     }
 }
 
-export default Home;
+export default About;
