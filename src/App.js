9ee2d6a0c62051418aba16198f192ea7d5bc526f
Akash <subs@appsparkler.com>
2019-06-28 12:37:44 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 
 // import all required libraries
 import React from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
-
-
+import { BrowserRouter } from 'react-router-dom'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
+//
+import Navbar from './components/Navbar'
+//
+import Home from './components/Home'
 
 function App() {
   return (
-    <h1><i>Poke Times Of India...</i></h1>
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar />
+      </div>
+    </BrowserRouter>
   );
 }
 
