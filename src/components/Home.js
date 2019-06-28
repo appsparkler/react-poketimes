592591b68872b0dbc035e4e0b6d2cee5b710b1d5
Akash <subs@appsparkler.com>
2019-06-28 10:17:30 +0530

    WIP

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+export default function(props) {
+    return {
+      <div className="container">
+        <h4 className="center">Home Page</h4>
+        <p>
+          
+        </p>
+      </div>
+    }
+}
