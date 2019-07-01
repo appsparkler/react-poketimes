a8062edfa3d3045e535781f72f1fe1f132819602
Akash <subs@appsparkler.com>
2019-07-01 11:28:23 +0530

    WIP

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import Rainbow from '../hoc/Rainbow'
 const About =() => {
     return (
       <div className="container">
-        <h4 className="center">About</h4>
+        <h4 className="center">About (Has Higher Order)</h4>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo sit accusamus ducimus quod possimus, quas necessitatibus laborum? Inventore error hic cupiditate saepe quasi quidem facilis, temporibus vero harum, repellat ab, nostrum corporis voluptatibus magni dignissimos delectus tempora? Animi consequatur modi rerum aperiam ea magni, quos, soluta suscipit voluptatum officiis at explicabo consectetur dolor aliquid quidem mollitia. Enim quia nemo aperiam quidem incidunt consequuntur. Consequuntur ut amet tenetur iure modi itaque nostrum, nam alias voluptates officiis atque aliquam velit vero repellendus molestias odit ducimus quia. Velit earum ab, magnam tempora perferendis mollitia sapiente sint, aliquid consequuntur eum unde dicta, repellendus voluptas!
         </p>
